14c42c12000f136593a648cc041cbe6dc8b4abdc
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-05-19 01:02:18 +0530

    Update serailizers.py

--- mypro/myapp2/serailizers.py
+++ mypro/myapp2/serailizers.py
@@ -5,4 +5,4 @@ from .models import Family
 class FamilySerlizer(serializers.ModelSerializer):
     class Meta:
         model = Family
-        fields = '__all__'  .....#fifol
+        fields = '__all__'  
